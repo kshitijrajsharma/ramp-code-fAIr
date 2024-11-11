74a97ee11458494a1c5e7459edc8f7e01c36e9ac
kshitijrajsharma <skshitizraj@gmail.com>
2024-11-11 11:37:26 +0100

    add F1_Score metric and additional accuracy functions for model evaluation

--- ramp/training/loss_constructors.py
+++ ramp/training/loss_constructors.py
@@ -7,49 +7,59 @@
 
 # adding logging
 import logging
+
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
-import tensorflow as tf
 import keras.backend as K
-from tensorflow.keras.losses import CategoricalCrossentropy, categorical_crossentropy
-
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.losses import CategoricalCrossentropy, categorical_crossentropy
 
 
 def get_sparse_categorical_crossentropy_fn(cfg):
-    '''
-    returns sparse categorical cross entropy. Does not use parameters. 
-    '''
+    """
+    returns sparse categorical cross entropy. Does not use parameters.
+    """
     return tf.keras.losses.sparse_categorical_crossentropy
 
-def get_custom_loss_fn(cfg):    
-    '''
+
+def get_custom_loss_fn(cfg):
+    """
     returns the Google Africa custom loss function with given parameters.
-    sigma: length scale for the gaussian-filtered weight image in the weighted scce loss. 
+    sigma: length scale for the gaussian-filtered weight image in the weighted scce loss.
     myconst: constant multiple for the gaussian-filtered weight image in the weighted scce loss.
     gamma (real, >0): FTL power parameter.
     alpha (real, 0-1): trades off cost of FP vs FN. alpha larger => FP counts more.
     epsilon (real,>0): small positive number to stabilize FTL computations.
     ftl_wt (real): trades off FT loss vs wtd scce loss. flt_wt larger => FT loss counts more.
-    '''           
-    
+    """
+
     ftl_wt = cfg["loss"]["loss_fn_parms"]["ftl_wt"]
 
     def custom_loss_fn(y_true, y_hat):
         wscce_loss = get_weighted_SCCE_loss_fn(cfg)
         ft_loss = get_focal_tversky_loss_function(cfg)
-        # return wscce_loss(y_true, y_hat) + 
-        return ftl_wt*ft_loss(y_true, y_hat)
+        # return wscce_loss(y_true, y_hat) +
+        return ftl_wt * ft_loss(y_true, y_hat)
+
     return custom_loss_fn
 
 
+def get_binary_crossentropy_fn(cfg):
+    return tf.keras.losses.BinaryCrossentropy()
+
+
+def get_categorical_crossentropy_fn(cfg):
+    return tf.keras.losses.CategoricalCrossentropy()
+
 
 def get_weighted_SCCE_loss_fn(cfg):
-    '''
+    """
     Returns a sparse categorical cross entropy loss function with per-pixel weighting.
-    '''
+    """
     from ..utils.imgproc_utils import batch_get_weight_image
+
     num_classes = cfg["num_classes"]
     sigma = cfg["loss"]["loss_fn_parms"]["sigma"]
     myconst = cfg["loss"]["loss_fn_parms"]["myconst"]
@@ -59,7 +69,7 @@ def get_weighted_SCCE_loss_fn(cfg):
 
         xentropy_loss = CategoricalCrossentropy()
 
-        # get the weighting image from the mask. 
+        # get the weighting image from the mask.
         # it has shape: (B,H,W)
         wt_img = batch_get_weight_image(y_true, sigma, myconst)
 
@@ -70,66 +80,69 @@ def get_weighted_SCCE_loss_fn(cfg):
     return weighted_SCCE_loss
 
 
-
 def get_SCCE_labelsmoothing_loss_fn(cfg):
-    #num_classes, ls_param=0.1):
-    '''
+    # num_classes, ls_param=0.1):
+    """
     Returns a sparse categorical cross entropy loss function that applies label smoothing.
-    '''
+    """
     num_classes = cfg["num_classes"]
     ls_param = cfg["loss"]["loss_fn_parms"]["ls_param"]
+
     def SCCE_labelsmoothing(y_true, y_hat):
         yt = tf.squeeze(tf.one_hot(tf.cast(y_true, tf.int32), num_classes))
-        return categorical_crossentropy(yt, y_hat, label_smoothing = ls_param)
+        return categorical_crossentropy(yt, y_hat, label_smoothing=ls_param)
 
     return SCCE_labelsmoothing
 
 
 def get_class_tversky_fn(alpha, epsilon):
-    
+
     def class_tversky(y_true, y_pred):
-        '''
+        """
         Returns a vector with length equal to the number of classes.
         Each class contributes to the total FTL in a 1-vs-rest manner.
-        '''
+        """
 
-        # Note: batch_flatten actually flattens all the other dimensions. 
+        # Note: batch_flatten actually flattens all the other dimensions.
         # So, for example, a (3, 5, 6, 2)-tensor becomes a (3, 60)-tensor.
-        # In this case, because you permuted channels and batches, you 
+        # In this case, because you permuted channels and batches, you
         # get a (C, HWB)-tensor.
         y_true_pos = K.batch_flatten(y_true)
         y_pred_pos = K.batch_flatten(y_pred)
 
-        # Note that each of these sums is over the HWB dimension, so you 
-        # get a C-vector in each case. 
-        # TP(c): terms are 0 if true class isn't c, large if the true class was 
+        # Note that each of these sums is over the HWB dimension, so you
+        # get a C-vector in each case.
+        # TP(c): terms are 0 if true class isn't c, large if the true class was
         # c and you got it right.
         true_pos = K.sum(y_true_pos * y_pred_pos, 1)
-        
-        # FN(c): terms are 0 if true class isn't c, large if the true class was 
+
+        # FN(c): terms are 0 if true class isn't c, large if the true class was
         # c and you got it wrong.
-        false_neg = K.sum(y_true_pos * (1-y_pred_pos), 1)
-        
-        # FP(c): terms are 0 if true class is c. and large if the true class 
+        false_neg = K.sum(y_true_pos * (1 - y_pred_pos), 1)
+
+        # FP(c): terms are 0 if true class is c. and large if the true class
         # wasn't c, but you guessed that it was.
-        false_pos = K.sum((1-y_true_pos)*y_pred_pos, 1)
-        
+        false_pos = K.sum((1 - y_true_pos) * y_pred_pos, 1)
+
         # alpha trades off FP and FN.
         # epsilon stabilizes the division.
-        return (true_pos + epsilon)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + epsilon)
+        return (true_pos + epsilon) / (
+            true_pos + alpha * false_neg + (1 - alpha) * false_pos + epsilon
+        )
 
     return class_tversky
 
+
 def get_focal_tversky_loss_function(cfg):
-    #num_classes, gamma, alpha, epsilon):
-    '''
+    # num_classes, gamma, alpha, epsilon):
+    """
     returns a FTL function with the given parameters.
-    num_classes (integer): # classes in the mask. 
+    num_classes (integer): # classes in the mask.
     gamma (real): FTL main parameter.
     alpha (real): trades off cost of FP vs FN.
     epsilon (real): small factor to stabilize FTL computations.
-    '''
-    
+    """
+
     num_classes = cfg["num_classes"]
     gamma = cfg["loss"]["loss_fn_parms"]["gamma"]
     alpha = cfg["loss"]["loss_fn_parms"]["alpha"]
@@ -138,23 +151,23 @@ def get_focal_tversky_loss_function(cfg):
     class_tversky = get_class_tversky_function(alpha, epsilon)
 
     def focal_tversky_loss(y_true, y_pred):
-        '''
+        """
         returns the sum of the classwise losses, raised to the gamma
         power to make it nonlinear.
-        '''
-        # y_true and y_pred must be one-hot encoded. 
-        # usually, y_true is sparse-encoded, and y_pred is one-hot encoded. 
+        """
+        # y_true and y_pred must be one-hot encoded.
+        # usually, y_true is sparse-encoded, and y_pred is one-hot encoded.
         # so transform y_true first.
         yt = tf.one_hot(tf.cast(tf.squeeze(y_true, 3), tf.int32), num_classes)
 
         # for tensors of shape batch-H-W-channels, this trades
         # the batch and channels dims, so you get a channels-H-W-batch shape.
-        yt = K.permute_dimensions(yt, (3,1,2,0))
-        y_pred = K.permute_dimensions(y_pred, (3,1,2,0))
-        
+        yt = K.permute_dimensions(yt, (3, 1, 2, 0))
+        y_pred = K.permute_dimensions(y_pred, (3, 1, 2, 0))
+
         # calculate classwise components of FTL
         pt_1 = class_tversky(yt, y_pred)
-        return K.sum(K.pow((1-pt_1), gamma))
+        return K.sum(K.pow((1 - pt_1), gamma))
 
     return focal_tversky_loss
 
@@ -163,27 +176,28 @@ def main():
 
     # unit test
     num_classes = 3
-    y_true = np.array((0,1,2,0,1,2,0,1,2))
+    y_true = np.array((0, 1, 2, 0, 1, 2, 0, 1, 2))
 
-    # this guesses 0 a lot, and 2, hardly ever. 
+    # this guesses 0 a lot, and 2, hardly ever.
     y_pred_0 = np.array((0.25, 0.25, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25))
     y_pred_1 = np.array((0.5, 0.5, 0.25, 0.25, 0.25, 0.25, 0.25, 0.5, 0.25))
     y_pred_2 = np.array((0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.5))
 
     # shape is (9, 3) -- classes last as comes out of the NN
-    y_pred = np.array((y_pred_0, y_pred_1, y_pred_2)).astype('float32').transpose()
-    
+    y_pred = np.array((y_pred_0, y_pred_1, y_pred_2)).astype("float32").transpose()
 
     # one hot encode the truth vector
     yt = tf.squeeze(tf.one_hot(tf.cast(y_true, tf.int32), num_classes))
-    
-    yt = K.permute_dimensions(yt, (1,0))
-    yp = K.permute_dimensions(y_pred, (1,0))
+
+    yt = K.permute_dimensions(yt, (1, 0))
+    yp = K.permute_dimensions(y_pred, (1, 0))
 
     class_tversky = get_class_tversky_function(alpha=0.5, epsilon=0)
     CT = class_tversky(yt, yp)
-    truth = np.array((0.38462, 0.41667,0.36364))
-    assert np.sum(np.square(CT - truth)) < 10E-06, f"returned unexpected value from class_tversky(): {CT}"
+    truth = np.array((0.38462, 0.41667, 0.36364))
+    assert (
+        np.sum(np.square(CT - truth)) < 10e-06
+    ), f"returned unexpected value from class_tversky(): {CT}"
 
     FT_loss = get_focal_tversky_loss_function(3, 1.0, 0.5, 0.0)
     # reshape y_true and y_pred so they look like tf tensors
@@ -194,17 +208,10 @@ def main():
     FTL = FT_loss(y_true, y_pred)
     truth_ftl = 1.83507
     print(FTL)
-    assert np.square(FTL - truth_ftl) < 10E-06, f"returned unexpected FTL: {FTL}"
-    
+    assert np.square(FTL - truth_ftl) < 10e-06, f"returned unexpected FTL: {FTL}"
 
     return
 
 
-    
-
-
-
-
-
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
--- ramp/training/metric_constructors.py
+++ ramp/training/metric_constructors.py
@@ -5,11 +5,101 @@
 #
 #################################################################
 
-import tensorflow as tf
-# adding logging
+import functools
 import logging
-log = logging.getLogger(__name__)
+
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+log = logging.getLogger()
 log.addHandler(logging.NullHandler())
+
+
+# Credit : https://github.com/ciupava/ramp-code-fAIr/blob/7fee0ed584fcf7d93f558550ace299c21e9d60e2/ramp/training/metric_constructors.py
+class F1_Score(tf.keras.metrics.Metric):
+    #  from https://stackoverflow.com/a/64477522
+    def __init__(self, class_id, name="f1_score", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.f1 = self.add_weight(name="f1", initializer="zeros")
+        self.precision_fn = tf.keras.metrics.Precision(class_id=class_id)
+        self.recall_fn = tf.keras.metrics.Recall(class_id=class_id)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        p = self.precision_fn(y_true, y_pred)
+        r = self.recall_fn(y_true, y_pred)
+        self.f1.assign(tf.math.divide_no_nan(2 * (p * r), (p + r)))
+
+    def result(self):
+        return self.f1
+
+    def reset_states(self):
+        # we also need to reset the state of the precision and recall objects
+        self.precision_fn.reset_states()
+        self.recall_fn.reset_states()
+        self.f1.assign(0)
+
 
 def get_sparse_categorical_accuracy_fn(cfg):
     return tf.keras.metrics.SparseCategoricalAccuracy()
+
+
+def get_categorical_accuracy_fn(cfg):
+    return tf.keras.metrics.CategoricalAccuracy()
+
+
+# ---
+# TODO: manually changing here for OHE experiment
+def get_iou_fn(cfg):
+    return tf.keras.metrics.IoU(num_classes=4, target_class_ids=[0, 1], name="iou")
+
+
+# ---
+
+
+def get_onehotiou_fn(cfg):
+    return tf.keras.metrics.OneHotIoU(
+        num_classes=4, name="ohe_iou", target_class_ids=[0, 1]
+    )
+
+
+# ---
+# TODO: manually changing here for OHE experiment
+def get_precision_fn(cfg):
+    return tf.keras.metrics.Precision(
+        # E.g. buildings
+        class_id=1,
+        name="precision_1",
+        # thresholds=None, top_k=None, class_id=None, name=None, dtype=None
+    )
+
+
+def get_recall_fn(cfg):
+    return tf.keras.metrics.Recall(
+        # E.g. buildings
+        class_id=1,
+        name="recall_1",
+        # thresholds=None,top_k=None, class_id=None, name=None, dtype=None
+    )
+
+
+def get_f1_score_fn(cfg):
+    return F1_Score(class_id=1)
+
+
+def get_mse_fn(cfg):
+    return tf.keras.losses.MeanSquaredError(
+        # reduction=losses_utils.ReductionV2.AUTO,
+        name="mean_squared_error"
+    )
+
+
+def get_accuracy_fn(cfg):
+    return tf.keras.metrics.Accuracy()
+
+
+def get_sparse_iou_fn(cfg):
+    return tf.keras.metrics.IoU(sparse_y_true=True, sparse_y_pred=False)
+
+
+def get_meanIoU_fn(cfg):
+    return tf.keras.metrics.MeanIoU(num_classes=2)
